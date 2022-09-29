5d515dd863fe39db70010afeab4ce99bfd91243c
lokinegalur <negalurlohith@gmail.com>
2022-09-29 08:06:30 +0000

    new dag

--- dags/example3_dag_test.py
+++ dags/example3_dag_test.py
@@ -16,6 +16,6 @@ import internal_unit_testing
 
 
 def test_dag_import():
-    from . import example_dag
+    from . import example3_dag
 
     internal_unit_testing.assert_has_valid_dag(example3_dag)
